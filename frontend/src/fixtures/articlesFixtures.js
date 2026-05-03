ac27b5c72471888e80799c653718739b56d12ceb
Wilson Lee <wilsonzlee@ucsb.edu>
2026-05-02 22:51:01 -0700

    wl - reformat

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,38 +1,39 @@
 const articlesFixtures = {
-    oneArticle: {
-        "id": 1,
-        "title": "Term in Review: A.S. EVPSA Leiya Kadah",
-        "url": "https://dailynexus.com/2026-05-01/term-in-review-evpsa-leiya-kadah/",
-        "explanation": "Leiya Kadah reflected on her term",
-        "email": "wilsonzlee@ucsb.edu",
-        "dateAdded": "2026-05-01T12:00:00"
+  oneArticle: {
+    id: 1,
+    title: "Term in Review: A.S. EVPSA Leiya Kadah",
+    url: "https://dailynexus.com/2026-05-01/term-in-review-evpsa-leiya-kadah/",
+    explanation: "Leiya Kadah reflected on her term",
+    email: "wilsonzlee@ucsb.edu",
+    dateAdded: "2026-05-01T12:00:00",
+  },
+  threeArticles: [
+    {
+      id: 1,
+      title: "Term in Review: A.S. EVPSA Leiya Kadah",
+      url: "https://dailynexus.com/2026-05-01/term-in-review-evpsa-leiya-kadah/",
+      explanation: "Leiya Kadah reflected on her term",
+      email: "wilsonzlee@ucsb.edu",
+      dateAdded: "2026-05-01T12:00:00",
     },
-    threeArticles: [
-        {
-            "id": 1,
-            "title": "Term in Review: A.S. EVPSA Leiya Kadah",
-            "url": "https://dailynexus.com/2026-05-01/term-in-review-evpsa-leiya-kadah/",
-            "explanation": "Leiya Kadah reflected on her term",
-            "email": "wilsonzlee@ucsb.edu",
-            "dateAdded": "2026-05-01T12:00:00"
-        },
-        {
-            "id": 2,
-            "title": "Exploring ENGL 106CW, the Catalyst",
-            "url": "https://dailynexus.com/2026-05-01/exploring-engl-106cw-the-catalyst/",
-            "explanation": "The Catalyst is a contemporary literary arts magazine",
-            "email": "maliaguy@ucsb.edu",
-            "dateAdded": "2026-05-01T13:00:00"
-        },
-        {
-            "id": 3,
-            "title": "Roger’s Tacos rents food truck, resumes service in I.V.",
-            "url": "https://dailynexus.com/2026-05-01/rogers-tacos-rents-food-truck-resumes-service-in-i-v/",
-            "explanation": "Roger’s Tacos is continuing service in Isla Vista through a newly rented food truck. ",
-            "email": "elijahobando@ucsb.edu",
-            "dateAdded": "2026-05-01T14:00:00"
-        }
-    ],
+    {
+      id: 2,
+      title: "Exploring ENGL 106CW, the Catalyst",
+      url: "https://dailynexus.com/2026-05-01/exploring-engl-106cw-the-catalyst/",
+      explanation: "The Catalyst is a contemporary literary arts magazine",
+      email: "maliaguy@ucsb.edu",
+      dateAdded: "2026-05-01T13:00:00",
+    },
+    {
+      id: 3,
+      title: "Roger’s Tacos rents food truck, resumes service in I.V.",
+      url: "https://dailynexus.com/2026-05-01/rogers-tacos-rents-food-truck-resumes-service-in-i-v/",
+      explanation:
+        "Roger’s Tacos is continuing service in Isla Vista through a newly rented food truck. ",
+      email: "elijahobando@ucsb.edu",
+      dateAdded: "2026-05-01T14:00:00",
+    },
+  ],
 };
 
 export { articlesFixtures };
