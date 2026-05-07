61eb9fce0f7b88d241cc60937a710d829a1e7718
Rushabh Taneja <rushabh@ucsb.edu>
2026-05-07 13:41:14 -0700

    rt- bug fixes, syntax error with storyboard

--- frontend/src/stories/pages/UCSBOrganization/UCSBOrganizationCreatePage.stories.jsx
+++ frontend/src/stories/pages/UCSBOrganization/UCSBOrganizationCreatePage.stories.jsx
@@ -28,9 +28,7 @@ Default.parameters = {
       });
     }),
     http.post("/api/ucsborganization/post", () => {
-      return HttpResponse.json(ucsbOrganizationFixtures.oneOrganization, {
-        status: 200,
-      });
+      return HttpResponse.json(ucsbOrganizationFixtures.oneOrganization, { status: 200 });
     }),
   ],
 };
