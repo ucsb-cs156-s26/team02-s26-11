f2706a2c62b2a86194587d8972eb5491419a559a
Nini Yen <h_yen@ucsb.edu>
2026-05-06 18:32:59 -0700

    Add UCSBDiningCommonsMenuItem fixtures

---
+++ frontend/src/main/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,33 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneItem: {
+    id: 1,
+    diningCommonsCode: "dlg",
+    name: "Spaghetti",
+    station: "Entree",
+  },
+
+  threeItems: [
+    {
+      id: 1,
+      diningCommonsCode: "dlg",
+      name: "Spaghetti",
+      station: "Entree",
+    },
+
+    {
+      id: 2,
+      diningCommonsCode: "ortega",
+      name: "Chicken Tacos",
+      station: "Grill",
+    },
+
+    {
+      id: 3,
+      diningCommonsCode: "carrillo",
+      name: "Caesar Salad",
+      station: "Salad Bar",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
