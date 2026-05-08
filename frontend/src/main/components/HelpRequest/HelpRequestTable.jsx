5b1c62194c11f96b13fb8fdf23553f7580967251
Brian Hu <bjhu4481cobra@gmail.com>
2026-05-07 19:02:08 -0700

    Update HelpRequestTable.jsx
    Readded S

--- frontend/src/main/components/HelpRequest/HelpRequestTable.jsx
+++ frontend/src/main/components/HelpRequest/HelpRequestTable.jsx
@@ -12,7 +12,7 @@ export default function HelpRequestsTable({ requests, currentUser }) {
   const navigate = useNavigate();
 
   const editCallback = (cell) => {
-    navigate(`/helprequest/edit/${cell.row.original.id}`);
+    navigate(`/helprequests/edit/${cell.row.original.id}`);
   };
 
   // Stryker disable all : hard to test for query caching
